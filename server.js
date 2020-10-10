a71aae9e4b3f24082f8d333f71fdc547ee746c7f
Stefanie Serpa <smserpa@gmail.com>
2020-10-09 19:36:19 -0700

    updated paths to css and html files to run properly

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
-const express = require("express")
-const path = require("path")
-const app = express()
-const PORT = process.env.PORT || 3000
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname,"./public"), {extensions: "html"}));
+
+
+
 
 app.listen(PORT, () => 
     console.log(`Server listening at http://localhost:${PORT}`));
 
-
+fs.readFile("db.json", (error) => {
+    if (error) {
+        console.log(error);
+    }else{
+        console.log("Read notes file");
+    }
+});
 
 
 
