348c0d9feb1c2e2b1c2dd5e451593f774040be51
Stefanie Serpa <smserpa@gmail.com>
2020-11-22 11:45:16 -0800

    added html route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,6 +8,10 @@ module.exports = app => {
     })
   );
 
+  app.get("/", req, res => {
+    res.json(path.join(__dirname, "../public/index.html"));
+  });  
+
   app.get("*", (req, res) =>
     res.sendFile(path.join(__dirname, "../public/index.html"))
   );
